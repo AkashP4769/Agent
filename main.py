b2b04ca3399163c7944346137dff772dbd5ff387
AkashP4769 <AwesomeAkash47@gmail.com>
2026-06-17 22:17:08 +0530

    docs: add comment

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from ui import app
 
+# Tried but couldn't get the openvpn to connect to KV server
+# So i tried to use the local server of LM Studio instead which hosted a qwen3 4B model
+# And made a gradio interface to interact with it
+
 def main():
     app.launch(theme="ocean")
 
